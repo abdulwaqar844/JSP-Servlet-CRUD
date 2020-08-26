9617a7cc79871706315fcfd0890d46fa9ec130fb
abdulwaqar844 <abdulwaqar844@users.noreply.github.com>
2020-08-26 09:44:15 +0500

    SQL File added

---
+++ student.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 26, 2020 at 06:42 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logininfo`
+--
+
+CREATE TABLE `logininfo` (
+  `userid` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `logininfo`
+--
+
+INSERT INTO `logininfo` (`userid`, `email`, `password`) VALUES
+(3, 'waqar', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  `cellno` varchar(11) NOT NULL,
+  `dob` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `name`, `department`, `cellno`, `dob`) VALUES
+(33, 'Usman Khwaja', 'Technology', '45453', '2020-04-01'),
+(35, 'Fiyaz', 'Punjab police', '1122', '2020-05-01'),
+(36, 'Shahzad ALi', 'PMDU', '15', '2020-04-30'),
+(37, 'Faheem Ahmad', 'Driver', '5454', '2020-05-01'),
+(38, 'Hadia', 'Physician', '54654', '2020-05-01'),
+(39, 'Riyaz Mahmooad', 'IT', '545453', '2020-06-30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
